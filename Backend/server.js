b48bbc12c70f5fd8a007546ab08119c86193948d
unknown <james.r@kalvium.community>
2024-12-10 23:25:48 +0530

    created server

---
+++ Backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+
+app.get('/ping',(req,res) =>{
+    return res.send('pong')
+})
+
+const PORT = 8080
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
